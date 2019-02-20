26f0eaa3d3917cf5b402c04d61267fdac1b18af5
Juan Ongarini <juan.ongarini@synapse.com>
2019-02-20 18:56:24 -0300

    INICIO TARJETAS

--- src/app/tarjetas/tarjetas.component.ts
+++ src/app/tarjetas/tarjetas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+declare var electron: any;
+declare var require: any;
 
 @Component({
   selector: 'app-tarjetas',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetasComponent implements OnInit {
 
-  constructor() { }
+	public ipc = electron.ipcRenderer;
+	public persona = this._appComponent.getPersonaActual();
+	public listTarjeta: Array<string>;
+	public listTarjetaConsumo: Array<string>;
 
-  ngOnInit() {
-  }
+	constructor(private ref: ChangeDetectorRef, private _appComponent: AppComponent) { }
 
+  	ngOnInit() {
+  		this.getTarjeta();
+  		this.getTarjetaConsumo();
+  	}
+
+  	getTarjeta(){
+		let me = this;
+		let personaActualID = this.persona[0];
+	    me.ipc.send("getTarjeta", personaActualID)
+	    me.ipc.on("resultSentTarjeta", function (evt, result) {
+			me.listTarjeta = [];
+			for (var i = 0; i < result.length; i++) {				
+				me.listTarjeta.push(result[i]);				
+			}
+			me.ref.detectChanges()
+	    });
+	}
+
+	getTarjetaConsumo(){
+		let me = this;
+		me.listTarjetaConsumo = [];
+		for (var i = 0; i < me.listTarjeta.length; i++) {				
+			let tarjetalID = me.listTarjeta[i][ID_TARJETA];
+		    me.ipc.send("getTarjetaConsumo", tarjetalID)
+		    me.ipc.on("resultSentTarjetaConsumo", function (evt, result) {
+				for (var i = 0; i < result.length; i++) {				
+					me.listTarjetaConsumo.push(result[i]);				
+				}
+				me.ref.detectChanges()
+		    });			
+		}
+	}
 }
